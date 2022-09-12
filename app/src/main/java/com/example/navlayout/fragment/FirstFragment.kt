f5aa390ab0dd5d50558bcc14d89f3cb4aae2f4eb
HusainLokhandwala14 <75034342+HusainLokhandwala14@users.noreply.github.com>
2022-09-13 02:31:25 +0530

    Fragment Containing Recycler View

--- app/src/main/java/com/example/navlayout/fragment/FirstFragment.kt
+++ app/src/main/java/com/example/navlayout/fragment/FirstFragment.kt
@@ -6,7 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.navlayout.R
+import com.example.navlayout.adapter.RVAdapter
 import com.example.navlayout.databinding.FragmentFirstBinding
 
 
@@ -17,12 +20,39 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val binding = DataBindingUtil.inflate<FragmentFirstBinding>(
+            layoutInflater, R.layout.fragment_first, container, false
+        )
+        return binding.root
 
-    val binding = DataBindingUtil.inflate<FragmentFirstBinding>(layoutInflater
-    , R.layout.fragment_first , container , false)
-
-    return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+
+        val rv: RecyclerView = view.findViewById(R.id.recycler_view)
+
+        rv.layoutManager = LinearLayoutManager(this.context)
+        val datafun = fetch()
+        val adap = RVAdapter(datafun)
+        rv.adapter = adap
+
+
+    }
+
+    private fun fetch(): ArrayList<String> {
+
+        val list = ArrayList<String>()
+        for (i in 0..100) {
+            list.add(" Number $i")
+
+        }
+        return list
+
+
+    }
 }
+
+
+
